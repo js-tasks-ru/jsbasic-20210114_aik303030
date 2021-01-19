09414e3339227f42ab9b4ca70549616e05305200
Каверина Анна <kaverina@immomsk.ru>
2021-01-19 11:10:09 +0300

    1-module 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,16 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  if(typeof n !== 'number' || n < 0) {
+    console.log('Введите целое неотрицательное число.');
+    return;
+  }
+
+  if(n === 0) return 1;
+
+  let result = n;
+  for (let i = n - 1; i >= 1; i--) {
+    result *= i;
+  }
+  return result;
 }
