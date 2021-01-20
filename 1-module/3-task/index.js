b1200c55483c9979ee87a4f5145aa1a875f80709
Anna <a.i.kaverina@mail.ru>
2021-01-20 22:22:49 +0300

    1-module 3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,5 +4,10 @@
  * @returns {string}
  */
 function ucFirst(str) {
-  // ваш код...
+  if(typeof str != 'string') {
+    console.log('Неправильный тип данных, введите строку.');
+    return;
+  }
+  if(str === '') return '';
+  return str[0].toUpperCase() + str.slice(1);
 }
