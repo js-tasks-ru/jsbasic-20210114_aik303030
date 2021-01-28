1db74bb6379d4a8f551026e713f2caf4e2403076
kaverina <aik303030@gmail.com>
2021-01-28 11:22:34 +0300

    3-module 5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,6 +3,17 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
 function getMinMax(str) {
-  // ваш код...
+  let numbers = str
+    .split(/\s|, */)
+    .filter(item => {
+      if (isFinite(item)) {
+        return parseFloat(item);
+      }
+    });
+  let result = {};
+  result.min = Math.min(...numbers);
+  result.max = Math.max(...numbers);
+  return result;
 }
