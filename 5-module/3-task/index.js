c90d459e4f6bf0f753e634933737a9dc0471aa62
Anna <a.i.kaverina@mail.ru>
2021-02-09 22:54:55 +0300

    5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector('.carousel__inner');
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const slides = document.querySelectorAll('.carousel__slide');
+
+  let slideIndex = 0;
+  arrowLeft.style.display = 'none';
+
+  function checkDisplay() {
+    if(slideIndex === 0) {
+      arrowLeft.style.display = 'none';
+    } else {
+      arrowLeft.style.display = '';
+    }
+  
+    if(slideIndex === 3) {
+      arrowRight.style.display = 'none';
+    } else {
+      arrowRight.style.display = '';
+    }
+  }  
+
+  arrowLeft.addEventListener('click', function() {
+    const slide = slides[slideIndex];
+    const width = slide.offsetWidth;
+    console.log(width);
+    carouselInner.style.transform = `translateX(${width}px)`;
+    slideIndex--;
+    checkDisplay();
+  });
+
+  arrowRight.addEventListener('click', function() {
+    const slide = slides[slideIndex];
+    const width = slide.offsetWidth;
+    console.log(width);
+    carouselInner.style.transform = `translateX(${-width}px)`;
+    slideIndex++;
+    checkDisplay();
+  });
 }
